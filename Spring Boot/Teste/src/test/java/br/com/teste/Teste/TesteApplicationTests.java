811c1ac04054710ce0287e8c2dc3298503a386da
tahkasai <tainakasai01@gmail.com>
2025-11-26 15:27:42 -0300

    feat: Adiciona arquivo base

---
+++ Spring Boot/Teste/src/test/java/br/com/teste/Teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.Teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
